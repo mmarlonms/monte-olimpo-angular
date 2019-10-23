c532fa2eb3856b0eee8c991d433df06d062ef664
Marlon Miranda <marlon_kkkk@hotmail.com>
2019-10-23 19:18:10 -0300

    Remoção de codigo desnecessário

--- src/src/app/pages/login/login.component.ts
+++ src/src/app/pages/login/login.component.ts
@@ -1,42 +1,23 @@
 import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/services/auth-service';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-    @Input() error: string | null;
-  
-    form: FormGroup = new FormGroup({
-        username: new FormControl(''),
-        password: new FormControl(''),
-    });
+export class LoginComponent  {
 
     constructor(
         private authService: AuthService,
         private router: Router,
     ) { }
 
-    ngOnInit() {
-    }
-
-    submit() {
-        if (this.form.valid) {
-            this.login(this.form.value.username,this.form.value.password)
-        }
-      }
-
     login(username: string, password: string) {
 
         this.authService.login(username, password).subscribe(
             success => this.router.navigate(['app/dashboard']),
-            error => this.error = error
         );
     }
-
 }
